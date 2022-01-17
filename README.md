dfae6a8d93d66f3fd27044dd1e11872f0fc24f9f
hasin-git <hasin03m@gmail.com>
2022-01-17 21:09:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1ckcuz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1ckcuz)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from "./App";
+//import { CounterComponent } from './CounterComponent';
+import AllProduct from './Product/AllProduct';
+
+ReactDOM.render(<AllProduct />, document.getElementById('root'));
